c8d9f1f30324b065140bb19afd44663dc8d2ee17
Rafliram <keerouk.ink@gmail.com>
2019-11-26 18:04:26 +0700

    (+) Fetch API Item (+) Fetch API Detail (+) Fetch API Confirm

---
+++ app/src/main/java/rpl/ezy/olread/model/MRecipe.kt
@@ -0,0 +1,10 @@
+package rpl.ezy.olread.model
+
+class MRecipe (
+    var recipe_id: Int,
+    var user_id: Int,
+    var title: String,
+    var recipe: String,
+    var img_url: String,
+    var isAccept: Int
+)
--- app/src/main/java/rpl/ezy/olread/view/AdminActivity.kt
+++ app/src/main/java/rpl/ezy/olread/view/AdminActivity.kt
@@ -1,13 +1,123 @@
 package rpl.ezy.olread.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_admin.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import rpl.ezy.olread.R
+import rpl.ezy.olread.adapter.RecyclerAcceptedRecipesAdapter
+import rpl.ezy.olread.adapter.RecyclerUnAcceptedRecipesAdapter
+import rpl.ezy.olread.api.GetDataService
+import rpl.ezy.olread.api.RetrofitClientInstance
+import rpl.ezy.olread.model.MRecipe
+import rpl.ezy.olread.response.ResponseRecipes
+import rpl.ezy.olread.utils.ConstantUtils
+import rpl.ezy.olread.utils.ConstantUtils.RECIPE_ID
+import rpl.ezy.olread.utils.SharedPreferenceUtils
+
 
 class AdminActivity : AppCompatActivity() {
+
+    private var sharedPreferences: SharedPreferenceUtils? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        sharedPreferences = SharedPreferenceUtils(this@AdminActivity)
+
+        AcceptedRecipe()
+        UnAcceptedRecipes()
+        bt_logout.setOnClickListener {
+            ActionLogout()
+        }
     }
+
+    fun AcceptedRecipe(){
+        Toast.makeText(
+            this@AdminActivity,
+            "Resume",
+            Toast.LENGTH_SHORT
+        ).show()
+        val service =
+            RetrofitClientInstance().getRetrofitInstance().create(GetDataService::class.java)
+        val call = service.getAcceptedRecipe()
+        call.enqueue(object : Callback<ResponseRecipes> {
+            override fun onFailure(call: Call<ResponseRecipes>, t: Throwable) {
+                Toast.makeText(
+                    this@AdminActivity,
+                    "Something went wrong...Please try later!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d("LOGLOGAN", "${t.message}")
+            }
+
+            override fun onResponse(call: Call<ResponseRecipes>,response: Response<ResponseRecipes>) {
+                if (response.body()!!.status == 200){
+                    var data = response.body()!!.data
+
+                    var mAdapter = RecyclerAcceptedRecipesAdapter(this@AdminActivity, data)
+
+                    rv_acceptedRecipe.apply {
+                        layoutManager = LinearLayoutManager(this@AdminActivity, LinearLayoutManager.HORIZONTAL, false)
+                        adapter = mAdapter
+                    }
+
+                }
+            }
+        })
+    }
+
+
+    fun UnAcceptedRecipes(){
+        val service =
+            RetrofitClientInstance().getRetrofitInstance().create(GetDataService::class.java)
+        val call = service.getUnAcceptedRecipe()
+        call.enqueue(object : Callback<ResponseRecipes> {
+            override fun onFailure(call: Call<ResponseRecipes>, t: Throwable) {
+                Toast.makeText(
+                    this@AdminActivity,
+                    "Something went wrong...Please try later!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d("LOGLOGAN", "${t.message}")
+            }
+
+            override fun onResponse(call: Call<ResponseRecipes>,response: Response<ResponseRecipes>) {
+                if (response.body()!!.status == 200){
+                    var data = response.body()!!.data
+
+                    var mAdapter = RecyclerUnAcceptedRecipesAdapter(this@AdminActivity, data)
+
+                    rv_unAcceptedRecipes.apply {
+                        layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+                        adapter = mAdapter
+                    }
+
+                }
+            }
+        })
+    }
+
+    fun ActionLogout() {
+        sharedPreferences!!.setSharedPreferences(ConstantUtils.USER_ID, -1)
+        sharedPreferences!!.setSharedPreferences(ConstantUtils.USERNAME, "")
+        sharedPreferences!!.setSharedPreferences(ConstantUtils.EMAIL, "")
+        sharedPreferences!!.setSharedPreferences(ConstantUtils.STATUS, "")
+        startActivity(Intent(this@AdminActivity, Authentification::class.java))
+        finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        AcceptedRecipe()
+        UnAcceptedRecipes()
+    }
+
 }
