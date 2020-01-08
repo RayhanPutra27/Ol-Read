94bc952974572124077242d24d76724e6157227b
rizqisatriawibowo <rizqisatriaw191@gmail.com>
2020-01-08 14:46:34 +0700

    (+)Add Recipes Activity

---
+++ app/src/main/java/rpl/ezy/olread/view/user/AddRecipesActivity.kt
@@ -0,0 +1,13 @@
+package rpl.ezy.olread.view.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import rpl.ezy.olread.R
+
+class AddRecipesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipes)
+    }
+}
